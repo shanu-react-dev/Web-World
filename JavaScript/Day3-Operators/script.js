b775991fc824ee27c7f68eba4fe48bbcfb1c45f9
shanu-react-dev <amarjeet.kr.dev@gmail.com>
2026-03-29 03:06:17 +0000

    increment and ternary operator

--- JavaScript/Day3-Operators/script.js
+++ JavaScript/Day3-Operators/script.js
@@ -46,4 +46,41 @@
 //todo 3. Relational Operators
 //todo 4. Logical Operators
 //todo 5. Unary Operators
+//? Unary Operators are operators which can be performed on the single operand. here we have typeof, increment and decrement
+//! Post Increment
+//? In post increment operator first the value is used then it updates the previous value.
+//! Pre Increment
+//? In pre increment operator the value is getting updated first then it is used.\
+
+//! Post Decrement
+//? In Post decrement operator firs the value will be used then it will be decreased by 1 after using the value
+//! pre Decrement
+//? In Pre Decrement operator firs the vlaue will get updated after this it will be used.
+
+let a = 7 // 8
+// console.log(a++)
+// console.log(a)
+let b = 10 //11
+// let expr = a++  + b++ +b - a // 7 + 10 + 11 - 8
+// console.log(expr)
+
+// let expr = --a + ++b - a + ++b + b
+// console.log(true++)
+
+
+// console.log(expr)
+
+// let isMarried = true
+// let expr = 1 + isMarried++ + ++1
+// console.log(expr)
 //todo 6. Ternary Operators
+//? Ternary Operators are also considered as conditional Operator and it shows or returns the output based on certain condtions
+let myName = "Shanu"
+myName === "Shan" ? console.log("I am Shanu") : console.log("I am not Shanu")
+
+let number = 19
+number % 2 === 0 ? console.log("The given number is even") : console.log("The given number is odd")
+// Prime Number
+//? A number which is divisible by one and itself apart from any other numbers is known as Prime Number.
+//e.g. 7 :- 1, 7
+//e.g. 10:- 1, 2, 5, 10
